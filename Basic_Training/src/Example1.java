b19b3cddf6a1f5bc8250c27925ca6cdaf27761a4
DISCOVER <DISCOVER@LAPTOP-G6009NKJ>
2019-06-23 13:53:57 +0530

    First Commit

---
+++ Basic_Training/src/Example1.java
@@ -0,0 +1,19 @@
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		// TODO items write here;
+		int rows = 5; 
+		//int n = 10;
+		for(int i=1;i<=rows;++i)
+		{
+			 for(int j=1;j<=i; ++j)
+			 {
+			System.out.print(j + " ");
+			 }
+			 System.out.println();
+		}
+
+	}
+
+}
